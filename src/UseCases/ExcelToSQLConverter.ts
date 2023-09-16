50866f2827873637886180cef34dc53b96256b23
nassorh <nassorh.dev@gmail.com@ --global>
2023-09-16 11:49:03 +0100

    Feat: ExcelToSQLConvertor use case completed and dependency injection completed

--- src/UseCases/ExcelToSQLConverter.ts
+++ src/UseCases/ExcelToSQLConverter.ts
@@ -1,15 +1,22 @@
 import IExcelParser from '../Adatpers/ExcelParser/IExcelParser';
+import ISQLStatementGenerator from '../Adatpers/SQLStatmentGenerators/ISQLStatementGenerator';
 
 class ExcelToSQLConverter {
   private excelParser: IExcelParser;
+  private sqlStatmentGenerator: ISQLStatementGenerator;
 
-  constructor(excelParser: IExcelParser) {
+  constructor(excelParser: IExcelParser, sqlStatmentGenerator: ISQLStatementGenerator) {
     this.excelParser = excelParser;
+    this.sqlStatmentGenerator = sqlStatmentGenerator
   }
 
-  convert(): void {
+  convert(fileBuffer: Buffer): string {
     // Parse data
+    const excelData = this.excelParser.parse(fileBuffer);
     // Convert data
+    const createTableStatment = this.sqlStatmentGenerator.generateCreateTableStatements(excelData);
+    const createInsertStatment = this.sqlStatmentGenerator.generateInsertStatements(excelData);
+    return createTableStatment + "\n" + createInsertStatment
   }
 }
 
